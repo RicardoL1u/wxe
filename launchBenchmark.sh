98e2fd0f0cfbc81cfe8c1efbf31f5f22ef02f6c4
wuxuaner <wuxuaner@infini-ai.com>
2024-04-12 11:57:22 +0800

    modify file

--- launchBenchmark.sh
+++ launchBenchmark.sh
@@ -29,4 +29,4 @@ DISTRIBUTED_ARGS="
     --master_addr $MASTER_ADDR \
     --master_port $MASTER_PORT"
 
-torchrun $DISTRIBUTED_ARGS benchmark.py -b 8M -e 1024M -f 2 -g 16
+torchrun $DISTRIBUTED_ARGS benchmarkComm.py -b 8M -e 1024M -f 2 -g 16
