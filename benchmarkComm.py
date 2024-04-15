2ed9b4227d049e64b707e7081a77ce24c4bac45e
wuxuaner <wuxuaner@infini-ai.com>
2024-04-15 11:21:17 +0800

    delete all_gather

--- benchmarkComm.py
+++ benchmarkComm.py
@@ -74,13 +74,13 @@ def benchmark_nccl_communication(begin_size, end_size, factor, gpus_node, num_te
         dist.barrier()   
         duration = (time.time() - start_time) / num_tests
 
-        start_time_gather = time.time()
-        for _ in range(num_tests):
-            dist.all_gather(gathered_tensors, tensor_copy)
-        dist.barrier()
-        elapsed_time = (time.time() - start_time_gather)/num_tests
-        if rank == 0:
-            print(f"Elapsed time: {elapsed_time} seconds")
+        # start_time_gather = time.time()
+        # for _ in range(num_tests):
+        #     dist.all_gather(gathered_tensors, tensor_copy)
+        # dist.barrier()
+        # elapsed_time = (time.time() - start_time_gather)/num_tests
+        # if rank == 0:
+        #     print(f"Elapsed time: {elapsed_time} seconds")
 
         
         if rank == 0:
@@ -88,9 +88,9 @@ def benchmark_nccl_communication(begin_size, end_size, factor, gpus_node, num_te
         # 每个节点的数据传输量为2 * N * S（其中N是节点数，S是数据大小）
         # total_data_per_node = 2 * dist.get_world_size() * size
         # 计算算法带宽（单位转换为Gigabytes per second）
-        algbw_gather = (size*(gpus_node-1) / elapsed_time) / 1e9
-        if rank == 0:
-            print(f"all_gather: Size: {size} bytes, Duration: {elapsed_time:.6f}s, Algbw: {algbw_gather:.2f} GB/s")
+        # algbw_gather = (size*(gpus_node-1) / elapsed_time) / 1e9
+        # if rank == 0:
+        #     print(f"all_gather: Size: {size} bytes, Duration: {elapsed_time:.6f}s, Algbw: {algbw_gather:.2f} GB/s")
         algbw = (size / duration) / 1e9 #计算方式存疑
         busbw = algbw * (2 * (gpus_node - 1) / gpus_node)
         if rank == 0:
