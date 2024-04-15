866f55d4df4c50060a9a0aafc146cd2304b62314
wuxuaner <wuxuaner@infini-ai.com>
2024-04-15 10:37:55 +0800

    test all_gather

--- benchmarkComm.py
+++ benchmarkComm.py
@@ -88,10 +88,13 @@ def benchmark_nccl_communication(begin_size, end_size, factor, gpus_node, num_te
         # 每个节点的数据传输量为2 * N * S（其中N是节点数，S是数据大小）
         # total_data_per_node = 2 * dist.get_world_size() * size
         # 计算算法带宽（单位转换为Gigabytes per second）
-        algbw_gather = (size*(dist.get_world_size()-1) / elapsed_time) / 1e9
+        algbw_gather = (size*(gpus_node-1) / elapsed_time) / 1e9
+        if rank == 0:
+            print(f"Size: {size} bytes, Duration: {elapsed_time:.6f}s, Algbw: {algbw_gather:.2f} GB/s")
         algbw = (size / duration) / 1e9 #计算方式存疑
         busbw = algbw * (2 * (gpus_node - 1) / gpus_node)
         if rank == 0:
+            
             print(f"Size: {size} bytes, Duration: {duration:.6f}s, Algbw: {algbw:.2f} GB/s, Busbw: {busbw:.2f} GB/s")
         size *= factor
 
